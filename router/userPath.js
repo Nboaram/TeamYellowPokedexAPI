74eafdcb27029d13ba15686ee976411cbc0dee7a
Daniel Little <daniel.r.little.1994@gmail.com>
2019-08-02 13:55:29 +0100

    made code neater

--- router/userPath.js
+++ router/userPath.js
@@ -22,14 +22,12 @@ router.put('/:username', async (request, response, next) => {
     schemas.UsersModel.findOneAndUpdate({username: request.params.username}, request.body).then(() => {
         response.status(202).send("update successful");
     });
-})
+});
 
 router.delete('/:username', (request, response, next) => {
     return schemas.UsersModel.findOneAndRemove({username: request.params.username}).then((x) =>{
         response.status(204).send(x);
     });
 });
-
-
 
 module.exports = router;
